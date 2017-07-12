814ce71e2e02d59184df7826b93fea8f78def1d9
kingleoric2010 <kingleoric2010@163.com>
2017-07-13 00:07:50 +0800

    four_2

--- mydjango/guest/guest/settings.py
+++ mydjango/guest/guest/settings.py
@@ -74,13 +74,26 @@ WSGI_APPLICATION = 'guest.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'NAME': 'guest',
+        'USER': 'root',
+        'PASSWORD': 'Pass1234',
+        #'OPTIONS': {'init_command': “SET sql_mode='STRICT_TRANS_TABLES'”,},
+    }
+}
+
 
 
 # Password validation
